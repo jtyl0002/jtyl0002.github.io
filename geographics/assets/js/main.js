ab285fffb5809987ba3ec51948492be11d958779
jtyl0002 <84952815+jtyl0002@users.noreply.github.com>
2021-06-15 22:48:45 +1000

    submit

--- geographics/assets/js/main.js
+++ geographics/assets/js/main.js
@@ -6,3 +6,28 @@ $( document ).ready(function() {
   });
   
 });
+
+$( document ).ready(function() {
+  console.log( "ready!" );
+
+$(".aboutchapter").click(function(){
+  $(".para2"). slideToggle("fast")
+});
+
+});
+
+window.onload = function() {
+
+  //add constraints object
+  var constraints = { audio: true,
+                      video: true};
+
+  //call getUserMedia, then the magic
+  navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream){
+      var video = document.querySelector('.webcam');
+      video.srcObject = mediaStream;
+      video.play();
+  }).catch(function(err){
+  });
+
+}
