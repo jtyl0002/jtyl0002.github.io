731397f8da886057e9236023833509e8f6981a93
jtyl0002 <84952815+jtyl0002@users.noreply.github.com>
2021-10-15 10:49:42 +1100

    m

---
+++ chrome reader/assets/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    console.log("ready");
+  
+    $(".face").click(function () {
+      $(".faceinfo").toggleClass("open");
+    });
+  });
+
+  $(document).ready(function () {
+    console.log("ready");
+  
+    $(".faceexit").click(function () {
+      $(".faceinfo").toggleClass("open");
+    });
+  });
+
+  $(document).ready(function () {
+    console.log("ready");
+  
+    $(".frog").click(function () {
+      $(".froginfo").toggleClass("open");
+    });
+  });
+
+  $(document).ready(function () {
+    console.log("ready");
+  
+    $(".frogexit").click(function () {
+      $(".froginfo").toggleClass("open");
+    });
+  });
